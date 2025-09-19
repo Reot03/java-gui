776c147c9ce2f7c59adbf46705834efaa03a8ffb
reot03 <104554570+Reot03@users.noreply.github.com>
2025-09-19 16:21:52 +0900

    Add files via upload

---
+++ Plate4x4Frame.java
@@ -0,0 +1,18 @@
+package prog09_05;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Plate4x4Frame extends JFrame { 
+	public Plate4x4Frame() {
+		super("2022011984-전우진");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		setLayout(new GridLayout(4,4));
+		Color [] color = {Color.RED,Color.orange, Color.YELLOW, Color.GREEN, Color.CYAN, Color.blue, Color.magenta, Color.gray, Color.pink, Color.LIGHT_GRAY};
+		for(int i=0; i<16; i++) {
+			JLabel label = new JLabel
+			
+		}
+	}
+}
